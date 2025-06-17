c4b5b994f744136cd42e95a054172a0fdc14d888
vedant553 <vedantnandeshwar2003@gmail.com>
2025-06-17 22:34:06 +0530

    Added Vite-based ReactJS-Practice-Programs folder for practicing

--- ReactJS-Practice-Programs/src/App.jsx
+++ ReactJS-Practice-Programs/src/App.jsx
@@ -2,9 +2,14 @@ import './App.css'
 
 function App() {
 
+  const seasons = ['Spring', 'Summer', 'Autumn', 'Winter'];
+
   return (
     <div>
-      <h1>Hello Duniyaa!</h1>
+      <h1>Seasons of the year</h1>
+      {seasons.map((season) => (
+        <div>{season}</div>
+      ))}
     </div>
   )
 }
